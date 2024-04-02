fix: Corrected model average calculations

u-net_workflow.py
@@ -26,11 +26,11 @@ model_configs = {
 
 classes = ['Land', 'Sand', 'Seaweed','Rock', 'Mussel', 'Salt marsh', 'Water', 'Seagrass', 'Cloud']
 
-MODEL = '1' # THIS IS THE MODEL TO BE RUN
+MODEL = '2' # THIS IS THE MODEL TO BE RUN
 
 
 # Displays the model success metrics and saves them to a file
-def evaluate_model_and_log(val_preds, val_target_img_paths, average_precision, tile, stage):
+def evaluate_tile(val_preds, val_target_img_paths, average_precision, tile, stage):
     average_f1=np.array([0,0,0,0,0,0,0,0,0])
     count_f1 = np.array([0,0,0,0,0,0,0,0,0])
     average_precision=np.array([0,0,0,0,0,0,0,0,0])
@@ -71,20 +71,36 @@ def evaluate_model_and_log(val_preds, val_target_img_paths, average_precision, t
         img = ImageOps.autocontrast(keras.utils.array_to_img(mask))
         img_name = val_target_img_paths[i].split('\\')[-1]
         img.save('.\\RESULTS\\model %s\\%s\\%s\\%s'%(MODEL, stage, tile, img_name))
-            
-    average_f1=average_f1 / count_f1
-    average_precision=average_precision / count_precision
-    average_recall=average_recall / count_recall
-    average_accuracy=average_accuracy / len(val_preds)
+    
+    average_f1 = average_f1 / count_f1
+    average_precision = average_precision / count_precision
+    average_recall = average_recall / count_recall
+    average_accuracy = average_accuracy / len(val_preds)
+    print("Tile f1: ", average_f1)
     print('{} accuracy: {}'.format(tile, average_accuracy))
     # Writes tile-wise log of results
-    with open('.\\RESULTS\\model %s\\%s\\%s\\evaluation.txt'%(MODEL ,stage,tile), 'w') as f:
+    with open('.\\RESULTS\\model %s\\%s\\%s\\evaluation.txt'%(MODEL,stage,tile), 'w') as f:
         f.write("              -- %s --- %s --- %s -- %s -- %s -- %s - %s - %s - %s \n"%(classes[0], classes[1], classes[2], classes[3], classes[4], classes[5], classes[6], classes[7], classes[8]))
         f.write("F1            %f  %f  %f  %f  %f  %f  %f  %f  %f \n"%(average_f1[0], average_f1[1], average_f1[2], average_f1[3], average_f1[4], average_f1[5], average_f1[6], average_f1[7], average_f1[8]))  # Writes F1 to log file
         f.write("Recall        %f  %f  %f  %f  %f  %f  %f  %f  %f \n"%(average_recall[0], average_recall[1], average_recall[2], average_recall[3], average_recall[4], average_recall[5], average_recall[6], average_recall[7], average_recall[8]))  # Writes F1 to log file
         f.write("Precision     %f  %f  %f  %f  %f  %f  %f  %f  %f \n"%(average_precision[0], average_precision[1], average_precision[2], average_precision[3], average_precision[4], average_precision[5], average_precision[6], average_precision[7], average_precision[8]))  # Writes F1 to log file
         f.write("Overall Accuracy %f \n"%(average_accuracy))  # Writes F1 to log file
-    return average_f1, average_recall, average_precision, average_accuracy, count_f1, count_recall, count_precision
+        
+    # When all patch values are nan for a class the average falsely defaults to zero which will impact model average
+    # Returning an array which detects invalid zero values
+    valid_f1s = [0,0,0,0,0,0,0,0,0]
+    valid_recalls = [0,0,0,0,0,0,0,0,0]
+    valid_precisions = [0,0,0,0,0,0,0,0,0]
+    for i in range(0, len(classes)):
+        if (count_f1[i] > 0):
+              valid_f1s[i] = 1
+    for i in range(0, len(classes)):
+        if (count_recall[i] > 0):
+              valid_recalls[i] = 1
+    for i in range(0, len(classes)):
+        if (count_precision[i] > 0):
+              valid_precisions[i] = 1
+    return average_f1, average_recall, average_precision, average_accuracy, valid_f1s, valid_recalls, valid_precisions
 
 def evaluate_model(image_paths, target_paths, model,stage):
      ## VALIDATES FOR REMAINDER OF EACH TRAINING TILE AND SAVES RESULT
@@ -99,20 +115,22 @@ def evaluate_model(image_paths, target_paths, model,stage):
         # Make predictions of segments and average_precision using the model on different subset of the tiles used in training
         val_preds = u_net.validate_model(image_paths[tile], target_paths[tile], model)
         # Evaluates the model
-        f1, recall, precision, accuracy, cnt_f1, cnt_recall, cnt_precision = evaluate_model_and_log(val_preds, target_paths[tile], MODEL, tile, stage)
-        print(f1, recall, precision, accuracy)
+        f1, recall, precision, accuracy, cnt_f1, cnt_recall, cnt_precision = evaluate_tile(val_preds, target_paths[tile], MODEL, tile, stage)
         average_f1 += f1
         average_recall += recall
         average_precision += precision
         average_accuracy += accuracy
-        count_all_f1 += cnt_f1
-        count_all_recall += cnt_recall
-        count_all_precision += cnt_precision
+        count_all_f1 += cnt_f1 # Counts how many valid f1s there are across tiles
+        count_all_recall += cnt_recall # Counts how many valid f1s there are across tiles
+        count_all_precision += cnt_precision # Counts how many valid f1s there are across tiles
+    
     average_f1=average_f1 / count_all_f1
     average_precision=average_precision / count_all_precision
     average_recall=average_recall / count_all_recall
     average_accuracy=average_accuracy / len(image_paths)
+    print("Model f1: ",average_f1)
     print('Overall accuracy: {}'.format(average_accuracy))
+    
     # Writes overall model log of results
     with open('.\\RESULTS\\model %s\\%s\evaluation.txt'%(MODEL, stage), 'w') as f:
         f.write("              -- %s --- %s --- %s -- %s -- %s -- %s - %s - %s - %s \n"%(classes[0], classes[1], classes[2], classes[3], classes[4], classes[5], classes[6], classes[7], classes[8]))
@@ -123,7 +141,7 @@ def evaluate_model(image_paths, target_paths, model,stage):
 
 def run_model_workflow():
 
-    ## GENRATES TRAINING/TESTING DATA AND GETS PATHS#
+    ## GENRATES TRAINING/TESTING DATA AND GETS PATHS
     training_paths, target_paths = ds.get_paths(model_configs[MODEL][1])
     train_dataset, valid_dataset, val_input_img_paths_by_tile, val_target_img_paths_by_tile = ds.train_test_split(training_paths, target_paths, model_configs[MODEL][0])
     
@@ -132,8 +150,8 @@ def run_model_workflow():
     ## RUNS THE MODEL
     # Train the model doing validation at the end of each epoch.
     base_model = u_net.train_base_model(train_dataset, valid_dataset, base_model, MODEL)
-
     evaluate_model(val_input_img_paths_by_tile, val_target_img_paths_by_tile, base_model, 'Training')
+    
     base_model = u_net.get_trained_model('model_' + MODEL + '.keras')
     ## GETS CONTROL IMAGES
     training_paths, target_paths = ds.get_paths(model_configs[MODEL][2])